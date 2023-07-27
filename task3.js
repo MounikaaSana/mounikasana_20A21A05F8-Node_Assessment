86131dca83bbe1a9513ad3c796eed9bad22630a8
MounikaaSana <104676932+MounikaaSana@users.noreply.github.com>
2023-07-27 12:52:24 +0530

    task3.js

--- task3.js
+++ task3.js
@@ -2,15 +2,15 @@ var nodemailer=require('nodemailer');
 var transporter=nodemailer.createTransport({
     service:'gmail',
     auth:{
-        user:"mounikasana80089@gmail.com",
-        pass:"uaoeuieimubebhgs"
+        user:'mounikasana80089@gmail.com',
+        pass:'uaoeuieimubebhgs'
     }
 });
 var mailOptions={
-    from:"mounikasana80089@gmail.com",
-    to:"pravin.consensus@gmail.com",
-    subject:"sending mail from node js",
-    text:"20A21A05F8"
+    from:'mounikasana80089@gmail.com',
+    to:'pravin.consensus@gmail.com',
+    subject:'sending mail from node js',
+    text:'20A21A05F8'
 };
 transporter.sendMail(mailOptions,function(error,info){
     if(error){
